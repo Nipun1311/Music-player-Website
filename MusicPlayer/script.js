4993b4b09eca935111f2ca96fcfff47b1310c2bc
Nipun1311 <162568542+Nipun1311@users.noreply.github.com>
2024-05-23 12:55:33 +0530

    Add files via upload

---
+++ MusicPlayer/script.js
@@ -0,0 +1,7 @@
+const wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: '#4F4A85',
+    progressColor: '#383351',
+    url: './songs/Akhiyaan Gulaab Teri Baaton Mein Aisa Uljha Jiya 320 Kbps.mp3',
+    barwidth:2,
+  })
